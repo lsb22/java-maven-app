c2f2783dbbfbd3676c8fb70ca2bfc32ebd611fe0
lsb <likith15600@gmail.com>
2025-10-29 17:14:01 +0530

    update version

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building docker image"
     withCredentials([usernamePassword(credentialsId:'docker-hub-repo', passwordVariable: 'PASS', usernameVariable:'USER')]) {
-        sh 'docker build -t lsb22/demo-app:jma-2.0 .'
+        sh 'docker build -t lsb22/demo-app:jma-2.1 .'
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push lsb22/demo-app:jma-2.0'
+        sh 'docker push lsb22/demo-app:jma-2.1'
     }
 }
 
